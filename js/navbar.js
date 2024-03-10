c89f9f41173334f320f21791cea68c8eb5d0b5a0
Peter-LLL <petliang@student.pakuranga.school.nz>
2024-03-11 09:37:08 +1300

    Fix navbar on mobile + simplify code for navbar. Started adding text + padding for text

--- js/navbar.js
+++ js/navbar.js
@@ -1,62 +1,19 @@
 var navbar = document.getElementById('navbar')
 var navbarpadcol = document.querySelectorAll('#navbar li a')
+var button = document.getElementById('drpbtn')
 
 function shrink(){
-    navbar.style.backgroundColor = "transparent";
-    navbar.style.fontSize = "25px";
-    navbar.style.width = "100%";
-    navbar.style.height = "auto";
-    for (var i = 0; i < navbarpadcol.length; i++) {
-        navbarpadcol[i].style.padding = "0.6vw 1.6vw";
-        navbarpadcol[i].style.color = "black";
-    }
+    navbar.classList.add('shrink');
 }
 
 function grow(){
-    navbar.style.backgroundColor = "#ADA3A4";
-    navbar.style.fontSize = "30px";
-    navbar.style.width = "100%";
-    navbar.style.height = "auto";
-    for (var i = 0; i < navbarpadcol.length; i++) {
-        navbarpadcol[i].style.padding = "1vw 2vw";
-        navbarpadcol[i].style.color = "white";
-    }
+    navbar.classList.remove('shrink');
 }
 
 function mobile(){
-    navbar.style.listStyleType = "none";
-    navbar.style.margin = "0";
-    navbar.style.padding = "0";
-    navbar.style.width = "32vw";
-    navbar.style.backgroundColor = "rgba(173, 163, 164, 0.5)";
-    navbar.style.position = "fixed";
-    navbar.style.height = "100%";
-    navbar.style.overflow = "hidden";
-    navbar.style.fontSize = "5vw";
-    for (var i = 0; i < navbarpadcol.length; i++) {
-        navbarpadcol[i].style.color = "white";
-    }
+    navbar.classList.add('mobile');
+    navbar.classList.remove('shrink');
 }
-
-function closeopen(){
-    if (navbar.style.width == "32vw") {
-        navbar.style.width = "0%";
-    } else if (navbar.style.width == "0%") {
-        navbar.style.width = "32vw";
-    }
-}
-
-window.addEventListener('resize', function(event) {
-    if (navbar.style.width != "0%" && this.innerWidth < 1000) {
-        mobile();
-        return;
-    }
-    if (window.scrollY >= 100 && this.innerWidth > 1000) {
-        shrink();
-    } else if (window.scrollY <= 0 && this.innerWidth > 1000) {
-        grow();
-    }  
-});
 
 window.addEventListener('scroll', function(event) {
     event.preventDefault(); 
