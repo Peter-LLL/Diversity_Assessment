509f8a7f0fc1af83d2fefe3e55a37a3adfb0a30b
Wadicul <petliang@student.pakuranga.school.nz>
2024-03-14 15:10:16 +1300

    Added pointer event so click does not register when the bak to top button is hidden

--- js/back.js
+++ js/back.js
@@ -4,8 +4,10 @@ window.addEventListener('scroll', function(event) {
     event.preventDefault(); 
     if (this.window.scrollY >= 200) {
         buttonTop.style.opacity = "1";  
+        buttonTop.style.pointerEvents = "auto";
     }
     else {
         buttonTop.style.opacity = "0";
+        buttonTop.style.pointerEvents = "none";
     }
 });
